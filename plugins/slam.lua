1059bc7f1ab863933f27c683f08acd4a7dda6d97
alicybery <alirezasalehijoker137889@gmail.com>
2016-04-10 17:41:45 +0430

    Create slam.lua

---
+++ plugins/slam.lua
@@ -0,0 +1,1 @@
+do function run(msg, matches) local reply_id = msg['211981574'] local text = 'سلام بابایی جونم😍' if matches[1] == 'salam' then if is_sudo(msg) then reply_msg(reply_id, text, ok_cb, false) end end end return { patterns = { "salam" }, run = run } end
